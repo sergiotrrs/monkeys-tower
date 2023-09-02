b95610cceaf53793edf905ae02f5d59bd04f0510
Sergio Torres <sergio.torres@idr.edu.mx>
2023-09-02 01:07:26 -0600

    feat: boilerplate springboot project

---
+++ src/main/java/com/mkstower/MonkeysTowerApplication.java
@@ -0,0 +1,13 @@
+package com.mkstower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonkeysTowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonkeysTowerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mkstower/MonkeysTowerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkstower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonkeysTowerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
